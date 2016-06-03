a91cce86d88ed584bf150da64d7c661844782fd6
Josiah Dudzik <jdudzik@velocity.org>
2016-06-03 16:27:56 -0400

    -Added timer for date and time. Added link for JS Trello board.

--- js/allyourbase.js
+++ js/allyourbase.js
@@ -1,1 +1,6 @@
-document.getElementById("theDate").innerHTML = Date();
+// Every 0.5 seconds, reload the current date and time on the page.
+setInterval(function(){ 
+	document.getElementById("theDate").innerHTML = Date();
+}, 500);
+
+
